5e25b9fbf1661ce2bb49ad239467d9e9911fbcb0
Ron Mallory <ronnymallory@gmail.com>
2023-09-12 11:58:38 -0700

    feat: modifying config and uploading analysis notebook for bank failures

--- src/config.py
+++ src/config.py
@@ -19,26 +19,6 @@ KAGGLE_METADATA = {
             "description": "Get detail on historical bank failures from 1934 to present.",
         },
         {
-            "path": "events_definitions.csv",
-            "description": "CSV definition file for bank events",
-        },
-        {
-            "path": "institutions.csv",
-            "description": "Get detail on all FDIC-insured institutions.",
-        },
-        {
-            "path": "institutions_definitions.csv",
-            "description": "CSV definition file for bank institutions",
-        },
-        {
-            "path": "locations.csv",
-            "description": "Get location details on all FDIC-insured institutions.",
-        },
-        {
-            "path": "locations_definitions.csv",
-            "description": "CSV definition file for bank locations",
-        },
-        {
             "path": "failure_properties.csv",
             "description": "/failures api definitions file converted from yaml to csv.",
         },
@@ -53,7 +33,6 @@ FAILURES_PARAMS = {
     "sort_order": "DESC",
     "format": "json",
     "download": "false",
-    "fields": "NAME,CERT,FIN,CITYST,FAILDATE,FAILYR,SAVR,RESTYPE1,CHCLASS1,RESTYPE,QBFDEP,QBFASSET,COST,PSTALP",
     "filename": "bank_failures",
 }
 FAILURES_DEFINITION_ENDPOINT = "/docs/failure_properties.yaml"
